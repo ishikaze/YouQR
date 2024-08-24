c074f0ab275c097de169e0d5daad6f92100b5709
Ishikaze K. <ishikaze.k@gmail.com>
2024-08-25 00:26:13 +0700

    better refresh

--- generate/presets.js
+++ generate/presets.js
@@ -1,43 +1,46 @@
-const refresh = document.getElementById('button-clear-corners-square-color')
+function refresh() {
+    document.getElementById('form-hide-background-dots').click()
+    document.getElementById('form-hide-background-dots').click()
+}
 
 function presetDefault() {
     document.getElementById('form-dots-type').value = 'square'
     document.getElementById('form-corners-square-type').value = 'square'
     document.getElementById('form-corners-dot-type').value = 'square'
-    refresh.click()
+    refresh()
 }
 
 function presetRounded() {
     document.getElementById('form-dots-type').value = 'rounded'
     document.getElementById('form-corners-square-type').value = 'extra-rounded'
     document.getElementById('form-corners-dot-type').value = 'dot'
-    refresh.click()
+    refresh()
 }
 
 function presetExtraRounded() {
     document.getElementById('form-dots-type').value = 'extra-rounded'
     document.getElementById('form-corners-square-type').value = 'extra-rounded'
     document.getElementById('form-corners-dot-type').value = 'dot'
-    refresh.click()
+    refresh()
 }
 
 function presetCircles() {
     document.getElementById('form-dots-type').value = 'dots'
     document.getElementById('form-corners-square-type').value = 'dot'
     document.getElementById('form-corners-dot-type').value = 'dot'
-    refresh.click()
+    refresh()
 }
 
 function presetClassy() {
     document.getElementById('form-dots-type').value = 'classy'
     document.getElementById('form-corners-square-type').value = 'extra-rounded'
     document.getElementById('form-corners-dot-type').value = 'dot'
-    refresh.click()
+    refresh()
 }
 
 function presetClassyRounded() {
     document.getElementById('form-dots-type').value = 'classy-rounded'
     document.getElementById('form-corners-square-type').value = 'extra-rounded'
     document.getElementById('form-corners-dot-type').value = 'dot'
-    refresh.click()
+    refresh()
 }

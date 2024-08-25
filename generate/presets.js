6aa3cfc914a05ab9a0d58dcc1ffb3cd8d3ddf9a1
Ishikaze K. <ishikaze.k@gmail.com>
2024-08-25 16:46:43 +0700

    quick styling

--- generate/presets.js
+++ generate/presets.js
@@ -44,3 +44,122 @@ function presetClassyRounded() {
     document.getElementById('form-corners-dot-type').value = 'dot'
     refresh()
 }
+
+
+document.getElementById('style-select').addEventListener('change', (e) => {
+    let presetValue = e.target.value;
+    switch(presetValue) {
+        case 'default':
+            presetDefault();
+            break;
+        case 'rounded':
+            presetRounded();
+            break;
+        case 'extraRounded':
+            presetExtraRounded();
+            break;
+        case 'circles':
+            presetCircles();
+            break;
+        case 'classy':
+            presetClassy();
+            break;
+        case 'classyRounded':
+            presetClassyRounded();
+            break;
+    };
+})
+
+document.getElementById('qr-color').addEventListener('change', (e) => {
+    let presetValue = e.target.value;
+    document.getElementById('form-dots-color').value = presetValue
+    document.getElementById('form-corners-square-color').value = presetValue
+    document.getElementById('form-corners-dot-color').value = presetValue
+    refresh()
+})
+
+document.getElementById('quick-background-color').addEventListener('change', (e) => {
+    let presetValue = e.target.value;
+    document.getElementById('form-background-color').value = presetValue
+    refresh()
+})
+
+document.getElementById('presets-select').addEventListener('change', (e) => {
+    let presetValue = e.target.value;
+    switch(presetValue) {
+        case 'default':
+            document.getElementById('form-background-color').value = '#FFFFFF'
+            document.getElementById('form-dots-color').value = '#000000'
+            document.getElementById('form-corners-square-color').value = '#000000'
+            document.getElementById('form-corners-dot-color').value = '#000000'
+            refresh()
+            document.getElementById('form-background-color-type-gradient').click()
+            document.getElementById('form-background-color-type-single').click()
+            break;
+        case 'default-invert':
+            document.getElementById('form-background-color').value = '#000000'
+            document.getElementById('form-dots-color').value = '#FFFFFF'
+            document.getElementById('form-corners-square-color').value = '#FFFFFF'
+            document.getElementById('form-corners-dot-color').value = '#FFFFFF'
+            refresh()
+            document.getElementById('form-background-color-type-gradient').click()
+            document.getElementById('form-background-color-type-single').click()
+            break;
+        case 'firery':
+            document.getElementById('bg-gradient1').value = '#821131'
+            document.getElementById('bg-gradient2').value = '#C7253E'
+            document.getElementById('form-dots-color').value = '#FABC3F'
+            document.getElementById('form-corners-square-color').value = '#FABC3F'
+            document.getElementById('form-corners-dot-color').value = '#FABC3F'
+            document.getElementById('form-background-gradient-rotation').value = '45'
+            refresh()
+            document.getElementById('form-background-color-type-single').click()
+            document.getElementById('form-background-color-type-gradient').click()
+            break;
+        case 'purple':
+            document.getElementById('bg-gradient1').value = '#2E073F'
+            document.getElementById('bg-gradient2').value = '#7A1CAC'
+            document.getElementById('form-dots-color').value = '#EBD3F8'
+            document.getElementById('form-corners-square-color').value = '#EBD3F8'
+            document.getElementById('form-corners-dot-color').value = '#EBD3F8'
+            document.getElementById('form-background-gradient-rotation').value = '45'
+            refresh()
+            document.getElementById('form-background-color-type-single').click()
+            document.getElementById('form-background-color-type-gradient').click()
+            break;
+        case 'sunset':
+            document.getElementById('bg-gradient1').value = '#FB773C'
+            document.getElementById('bg-gradient2').value = '#EB3678'
+            document.getElementById('form-dots-color').value = '#180161'
+            document.getElementById('form-corners-square-color').value = '#180161'
+            document.getElementById('form-corners-dot-color').value = '#180161'
+            document.getElementById('form-background-gradient-rotation').value = '270'
+            refresh()
+            document.getElementById('form-background-color-type-single').click()
+            document.getElementById('form-background-color-type-gradient').click()
+            break;
+        case 'lemon':
+            document.getElementById('bg-gradient1').value = '#F6E96B'
+            document.getElementById('bg-gradient2').value = '#BEDC74'
+            document.getElementById('form-dots-color').value = '#387F39'
+            document.getElementById('form-corners-square-color').value = '#387F39'
+            document.getElementById('form-corners-dot-color').value = '#387F39'
+            document.getElementById('form-background-gradient-rotation').value = '45'
+            refresh()
+            document.getElementById('form-background-color-type-single').click()
+            document.getElementById('form-background-color-type-gradient').click()
+            break;
+        case 'peach':
+            document.getElementById('bg-gradient1').value = '#FF7777'
+            document.getElementById('bg-gradient2').value = '#FFAAAA'
+            document.getElementById('form-dots-color').value = '#287B5B'
+            document.getElementById('form-corners-square-color').value = '#287B5B'
+            document.getElementById('form-corners-dot-color').value = '#287B5B'
+            document.getElementById('form-background-gradient-rotation').value = '45'
+            refresh()
+            document.getElementById('form-background-color-type-single').click()
+            document.getElementById('form-background-color-type-gradient').click()
+            break;
+    };
+    
+})
